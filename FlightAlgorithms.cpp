4eb32bc1626a314edf549a86add3e4a1af367e92
henrywu3 <henrywu3@illinois.edu>
2021-05-06 11:13:48 -0500

    Fixed comments for shortest path algs

--- FlightAlgorithms.cpp
+++ FlightAlgorithms.cpp
@@ -72,11 +72,9 @@ vector<string> FlightAlgorithms::dijkstra(int start, int dest) {
  
     int curr; //ID of current airport
 
-    //     while the top of priority_queue is not destination:
+    //     while the queue is not empty
     while (!airportQueue.empty()) {
-        //       get the current_node from priority_queue
-
-        //First we need to find the node in the queue that has the shortest distance from the beginning
+        //First we need to find the node in the queue that has the shortest distance from the beginning; simulate priority queue
         double min_dist = std::numeric_limits<double>::infinity(); //set initial distance to infinity
         int min_i; //stores the index of the node with the shortest distance from the start
         for (size_t i = 0; i < airportQueue.size(); i++) { // find node in queue with min distanceFromStart
@@ -99,7 +97,7 @@ vector<string> FlightAlgorithms::dijkstra(int start, int dest) {
             }
             // Check if next airport has not been already added to the queue before, or if it is in the queue...
             if (visited[it->first] == false || find(airportQueue.begin(), airportQueue.end(), it->first) != airportQueue.end()) {
-                double neighbor_dist = distances[curr] + (it->second).getWeight(); //  distance of node of current iteration for the start
+                double neighbor_dist = distances[curr] + (it->second).getWeight(); //  distance of node of current iteration from the start
                 //           if update its neighbor's distances:
                 if (neighbor_dist < distances[it->first]) {
                     distances[it->first] = neighbor_dist; //update neighbor's distance
@@ -125,35 +123,33 @@ vector<string> FlightAlgorithms::dijkstra(int start, int dest) {
 
 
 vector<string> FlightAlgorithms::A_star(int start, int dest) {
-    vector<double> gscore(14110); //Initialize distances
-    vector<double> fscore(14110);
+    vector<double> gscore(14110); // Initialize g-scores, which is distance from start of each node
+    vector<double> fscore(14110); // Initialize f-scores, which is heuristic; straight path of node to destination
     vector<int> previous(14110); // initialize a map that maps current node -> its previous node
 
-    vector<int> airportQueue;    // queue for Djikstra
+    vector<int> airportQueue;    // queue for A*
     airportQueue.push_back(start);   // enqueue first airport
     vector<bool> visited(14110);
     for (unsigned i = 0; i < 14110; i++) {
         visited[i] = false; //initialize all indices of visited to false
         previous[i] = -1;  //initialize all previous airports to none (-1)
-        gscore[i] = std::numeric_limits<double>::infinity(); //  set all distances to infinity
-        fscore[i] = std::numeric_limits<double>::infinity(); 
+        gscore[i] = std::numeric_limits<double>::infinity(); //  set all gscores to infinity
+        fscore[i] = std::numeric_limits<double>::infinity(); //  set all fscores to infinity
     }
 
     visited[start] = true; // set start to visited
     gscore[start] = 0; // set distance from start to start to be 0
-    fscore[start] = flightGraph_.calculateDistance(start, dest);
+    fscore[start] = flightGraph_.calculateDistance(start, dest); //set heuristic of start as the straight line distance from start to destination
     previous[start] = start; // initialize starting airport to start
  
     int curr; //ID of current airport
 
-    //     while the top of priority_queue is not destination:
+    //     while the queue is not empty
     while (!airportQueue.empty()) {
-        //       get the current_node from priority_queue
-
-        //First we need to find the node in the queue that has the shortest distance from the beginning
+        //First we need to find the node in the queue that has the shortest distance from the beginning; simulate priority queue
         double min_dist = std::numeric_limits<double>::infinity(); //set initial distance to infinity
         int min_i; //stores the index of the node with the shortest distance from the start
-        for (size_t i = 0; i < airportQueue.size(); i++) { // find node in queue with min distanceFromStart
+        for (size_t i = 0; i < airportQueue.size(); i++) { // find node in queue with min straight distance from the destination
             if (fscore[airportQueue[i]] < min_dist) { //if the airport id in the queue has a distance less than min distance...
                 min_dist = fscore[airportQueue[i]]; //Set new minimum distance
                 min_i = i; //set the minimum index to index within the queue that corresponds with the airport that has the minimum distance
@@ -168,16 +164,17 @@ vector<string> FlightAlgorithms::A_star(int start, int dest) {
         }
         //       for neighbor in current_node's neighbors and not in visited:
         for (auto it = flightGraph_.airportGraph[curr].airports.begin(); it != flightGraph_.airportGraph[curr].airports.end(); it++) {    //  search all departures from current airport
-            if (visited[it->first] == false) {  //  next airport has not been visited
+            if (visited[it->first] == false) {  //  next airport has not been added to the queue
                 airportQueue.push_back(it->first);   //  enqueue next airport
                 visited[it->first] = true;   // set to visited; added to queue already
             }
+            // Check if next airport has not been already added to the queue before, or if it is in the queue...
             if (visited[it->first] == false || find(airportQueue.begin(), airportQueue.end(), it->first) != airportQueue.end()) {
-                double tent_gscore = gscore[curr] + (it->second).getWeight(); //  distance of node of current iteration for the start
+                double tent_gscore = gscore[curr] + (it->second).getWeight(); //  distance of node of current iteration from the start
                 //           if update its neighbor's gscore:
                 if (tent_gscore < gscore[it->first]) {
                     gscore[it->first] = tent_gscore; //update neighbor's distance
-                    fscore[it->first] = tent_gscore + flightGraph_.calculateDistance(it->first, dest);
+                    fscore[it->first] = tent_gscore + flightGraph_.calculateDistance(it->first, dest); //update neighbor's heuristic
                     previous[it->first] = curr;  //    set previous[neighbor] = current_node
                 }
             }
