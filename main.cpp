69c8258c17d66e7ff4cd2e453a9ff97850469c48
Abhyan Jaikishen <abhyanj2@siebl-0224-09.ews.illinois.edu>
2021-05-08 02:33:56 -0500

    comment

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main() {
 	cout << "Enter a destination: ";
 	cin >> y;
 	vector<int> airports = fa.dijkstra(x, y);
-	//will lookup for airport codes later
+	//will lookup for airport codes later (can use 3830, 3361 for now) 
+	//Shortest path for ORD->SYD. Returns ORD->LAX->SYD
 	for (size_t i = 0;  i  < airports.size(); i++) {
 		cout << airports[i]; 
 		if(i < airports.size() - 1) {
